f4601cdabfe965a2da1e942865caec16c300f97f
Sebastian Ekström <seb.ekstrom@gmail.com>
2018-04-16 13:30:10 +0200

    Add logging in dev mode

--- src/tide.js
+++ src/tide.js
@@ -1,4 +1,4 @@
-import { Tide, initActions } from "tide";
+import { Tide, initActions, logging } from "tide";
 
 import State from "./state";
 import AppActions from "./actions/app";
@@ -7,5 +7,10 @@ export default function create() {
   const tide = new Tide();
   tide.setState(new State());
   initActions(tide, { app: AppActions });
+
+  if (process.env.REACT_APP_ENVIROMENT === "development") {
+    logging(tide, { actions: true, state: true, components: false });
+  }
+
   return tide;
 }
