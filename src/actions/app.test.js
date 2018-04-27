5bbc0a39ba44589094cd701bfb0839502114d122
Sebastian Ekström <seb.ekstrom@gmail.com>
2018-04-27 18:18:53 +0200

    Clean up tests

--- src/actions/app.test.js
+++ src/actions/app.test.js
@@ -2,14 +2,6 @@ import createTide from "../tide";
 import AppActions from "./app";
 
 describe("AppActions", () => {
-  let tide;
-  let actions;
-
-  beforeEach(() => {
-    tide = createTide();
-    actions = new AppActions(tide);
-  });
-
   test("load", () => {
     const tide = createTide();
     const actions = new AppActions(tide);
